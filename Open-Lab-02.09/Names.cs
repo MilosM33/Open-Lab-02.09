c7680d708ee245a82b949d7380a248bfa7cbb3de
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 13:46:33 +0200

    init

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_02._09
 {
     public class Names
     {
-        public string ConcatName(string firstName, string lastName)
-        {
-            throw new NotImplementedException();
-        }
+        public string ConcatName(string firstName, string lastName) => string.Format("{0}, {1}", lastName, firstName);
+        
     }
 }
